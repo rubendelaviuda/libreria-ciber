4b13b768128bfa56e0b4028dea4384344d89d68e
Rubén <91787030+rubendelaviuda@users.noreply.github.com>
2024-04-10 10:49:43 +0200

    Update libros.sql

--- libros.sql
+++ libros.sql
@@ -12,6 +12,9 @@ CREATE TABLE usuarios(
 	usuario VARCHAR(100) NOT NULL PRIMARY KEY,
     clave VARCHAR(255) NOT NULL,
     perfil VARCHAR(100) NOT NULL,
+	estado VARCHAR(10) NOT NULL,
+    numAccesosErroneos INTEGER,	
     fechaUltimoAcceso DATE
 );
-INSERT INTO `usuarios` (`usuario`, `clave`, `perfil`, `fechaUltimoAcceso`) VALUES ('root', '1234', 'admin', '2022-03-01');
+INSERT INTO `usuarios` (`usuario`, `clave`, `perfil`,`estado`, `numAccesosErroneos`,`fechaUltimoAcceso`) VALUES ('root', '$2b$10$JJdnLnCjYmNYpGQ8OJVUD.UmKx2jspOYnXOI9rZm5I9DX5GLYNUP6', 'admin', 'activo', 0, '2022-03-01 00:00');
+INSERT INTO `libros` (`id`, `nombre`, `descripcion`,`precio`, `autor`,`foto`) VALUES ('1', 'Default', 'Libro por defecto', '1', 'Ruben David y Hori', './libro1.png');
